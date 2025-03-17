43c44cad4d71f8b6858a9fa6eebed06a9858613e
Pedro Henrique <pedrohenriqueoliver2006@hotmail.com>
2025-03-16 22:30:41 -0300

    Add de operações

--- src/biblioteca/Operacoes.java
+++ src/biblioteca/Operacoes.java
@@ -5,4 +5,7 @@ public interface Operacoes {
     void listar();
     void modificar();
     void remover();
+    void emprestimoLivros();
+    void listarEmprestimos();
+    void devolverLivros();
 }
